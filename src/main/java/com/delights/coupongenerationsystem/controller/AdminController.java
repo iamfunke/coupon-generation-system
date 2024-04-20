d29f61955344ed2bea2c97f147664d8e210a3609
funke abolade <fa21aef@herts.ac.uk>
2024-04-20 22:53:17 +0100

    backend-update-7

--- src/main/java/com/delights/coupongenerationsystem/controller/AdminController.java
+++ src/main/java/com/delights/coupongenerationsystem/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.delights.coupongenerationsystem.controller;
 import com.delights.coupongenerationsystem.dto.response.ApiResponse;
 import com.delights.coupongenerationsystem.service.impl.AdminServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,14 @@ public class AdminController {
     @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping("retailers")
     public ResponseEntity<ApiResponse> getAllRetailers() {
-        return ResponseEntity.ok().body(adminService.getAllRetailers());
+        try {
+            return ResponseEntity.ok().body(adminService.getAllRetailers());
+        }catch (Exception e){
+            ApiResponse errorResponse = ApiResponse.builder()
+                    .success(false)
+                    .message("Unauthorised to access this resource ")
+                    .build();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+        }
     }
 }
