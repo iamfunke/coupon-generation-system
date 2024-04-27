21ae6e48a7e79cbf26777447df1c239ee2ca4885
funke abolade <fa21aef@herts.ac.uk>
2024-04-27 18:35:24 +0100

    Swagger Update

--- src/main/java/com/delights/coupongenerationsystem/CouponGenerationSystemApplication.java
+++ src/main/java/com/delights/coupongenerationsystem/CouponGenerationSystemApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "javainuseapi", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class CouponGenerationSystemApplication {
 
     public static void main(String[] args) {
