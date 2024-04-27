ab44802ba24c4790a4d18309d89f4f4014deef71
funke abolade <fa21aef@herts.ac.uk>
2024-04-27 18:39:15 +0100

    Swagger Update-2

--- src/main/java/com/delights/coupongenerationsystem/config/SwaggerConfig.java
+++ src/main/java/com/delights/coupongenerationsystem/config/SwaggerConfig.java
@@ -1,24 +1,20 @@
 package com.delights.coupongenerationsystem.config;
 
-import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -26,9 +22,7 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build()
-                .securitySchemes(Collections.singletonList(apiKey()))
-                .securityContexts(Collections.singletonList(securityContext()));
+                .build();
     }
 
     private ApiInfo apiInfo() {
@@ -39,18 +33,5 @@ public class SwaggerConfig {
                 .contact(new Contact("Funke Abolade", "https://www.linkedin.com/in/funke-abolade-a50682231", "agboluajevictoria@yahoo.com"))
                 .build();
     }
-
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", "Authorization", "header");
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(Collections.singletonList(defaultAuth()))
-                .build();
-    }
-
-    private SecurityReference defaultAuth() {
-        return new SecurityReference("JWT", new springfox.documentation.service.AuthorizationScope[0]);
-    }
 }
+
