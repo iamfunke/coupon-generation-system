5d862a7fc16e9ab9dce3e5e4b37ad840dffb1609
funke abolade <fa21aef@herts.ac.uk>
2024-04-27 18:45:11 +0100

    Swagger Update-3

--- src/main/java/com/delights/coupongenerationsystem/CouponGenerationSystemApplication.java
+++ src/main/java/com/delights/coupongenerationsystem/CouponGenerationSystemApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@SecurityScheme(name = "javainuseapi", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class CouponGenerationSystemApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/delights/coupongenerationsystem/config/SwaggerConfig.java
+++ src/main/java/com/delights/coupongenerationsystem/config/SwaggerConfig.java
@@ -1,20 +1,24 @@
 package com.delights.coupongenerationsystem.config;
 
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -22,7 +26,9 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()));
     }
 
     private ApiInfo apiInfo() {
@@ -32,6 +38,20 @@ public class SwaggerConfig {
                 .version("1.0-SNAPSHOT")
                 .contact(new Contact("Funke Abolade", "https://www.linkedin.com/in/funke-abolade-a50682231", "agboluajevictoria@yahoo.com"))
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Collections.singletonList(defaultAuth()))
+                .build();
+    }
+
+    private SecurityReference defaultAuth() {
+        return new SecurityReference("JWT", new springfox.documentation.service.AuthorizationScope[0]);
     }
 }
 
